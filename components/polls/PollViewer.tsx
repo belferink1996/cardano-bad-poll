0b808263d0f838f390b99c20e2db298e0f11ebfd
Ben Elferink <ben.elferink@icloud.com>
2023-02-16 18:51:22 +0200

    fix timer warning

--- components/polls/PollViewer.tsx
+++ components/polls/PollViewer.tsx
@@ -38,7 +38,7 @@ const PollViewer = (props: PollViewerProps) => {
   }, [poll])
 
   const timer = useTimer({
-    expiryTimestamp: new Date(poll.active ? poll.endAt : 0),
+    expiryTimestamp: new Date(poll.endAt),
     onExpire: () => callbackTimerExpired(),
   })
 
